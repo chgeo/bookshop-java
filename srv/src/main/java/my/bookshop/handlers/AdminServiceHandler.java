0bde308601a844eb5fecd7cbc4b05d627691e8de
Marc Becker <marc.becker@sap.com>
2019-11-27 09:31:22 +0100

    Extended NG handler (#31)
    * Calculate total and ensure correct OrderItem
    * Recalculate netAmount in case book is changed
    * Reduce database requests and calculate order's total
    * Remove LOG
    * Handle deletion of OrderItems and affect on total
    * Remove unnecessary files

--- srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
@@ -9,18 +9,15 @@ import java.util.stream.Stream;
 
 import javax.annotation.Resource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
 
 import com.sap.cds.Result;
 import com.sap.cds.ql.Select;
 import com.sap.cds.ql.Update;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.ServiceException;
-import com.sap.cds.services.cds.CdsCreateEventContext;
 import com.sap.cds.services.cds.CdsService;
+import com.sap.cds.services.draft.DraftCancelEventContext;
 import com.sap.cds.services.draft.DraftPatchEventContext;
 import com.sap.cds.services.draft.DraftService;
 import com.sap.cds.services.handler.EventHandler;
@@ -40,24 +37,18 @@ import adminservice.Orders_;
  * Custom handler for the Admin Service
  */
 @Component
-@RequestScope
 @ServiceName(AdminService_.CDS_NAME)
 public class AdminServiceHandler implements EventHandler {
 
-	/**
-	 * The logger
-	 */
-	private static final Logger LOG = LoggerFactory.getLogger(AdminServiceHandler.class.getName());
-
 	@Resource(name = AdminService_.CDS_NAME)
-	private CdsService adminService;
+	private DraftService adminService;
 
-
-	@Before(event = { CdsService.EVENT_CREATE }, entity = { Orders_.CDS_NAME } )
-	public void beforeCreateOrder(CdsCreateEventContext event, Stream<Orders> orders) throws Exception {
+	@Before(event = { CdsService.EVENT_CREATE, CdsService.EVENT_UPSERT })
+	public void beforeCreateOrder(Stream<Orders> orders) {
 		orders.forEach(order -> {
+			// reset total
+			order.setTotal(BigDecimal.valueOf(0));
 			order.getItems().forEach(orderItem -> {
-				LOG.info("Creating Order Item");
 				// validation of the request
 				Integer amount = orderItem.getAmount();
 				if (amount == null || amount <= 0) {
@@ -76,48 +67,97 @@ public class AdminServiceHandler implements EventHandler {
 					throw new ServiceException(ErrorStatuses.BAD_REQUEST, "Not enough books on stock");
 				}
 
-				// FIXME: this should not be necessary, as the net amount was already calculated in the
-				//   Before DRAFT_PATCH handler below. Unfortunately, the value will be set to null again,
-				//   because the field is marked read-only.
 				// update the net amount
 				BigDecimal updatedNetAmount = book.getPrice().multiply(BigDecimal.valueOf(amount));
 				orderItem.setNetAmount(updatedNetAmount);
+				// update the total
+				order.setTotal(order.getTotal().add(updatedNetAmount));
 
 				// update the book with the new stock
+				// FIXME this should only update the diff amount and handle book changes
 				book.setStock(book.getStock() - amount);
 				adminService.run(Update.entity(Books_.class).data(book));
 			});
 		});
 	}
 
-
-	@Before(event = { DraftService.EVENT_DRAFT_PATCH })
-	public void patchOrderItems(DraftPatchEventContext event, Stream<OrderItems> orderItems) throws Exception {
+	@Before(event = DraftService.EVENT_DRAFT_PATCH)
+	public void patchOrderItems(DraftPatchEventContext event, Stream<OrderItems> orderItems) {
 		orderItems.forEach(orderItem -> {
-			// check if amount was updated
+			// check if amount or book was updated
 			Integer amount = orderItem.getAmount();
-			if(amount == null) {
-				return;
+			String bookId = orderItem.getBookId();
+			String orderItemId = event.getParameterInfo().getQueryParameter(OrderItems.ID); // FIXME get from CQN
+			BigDecimal netAmount = calculateNetAmountInDraft(orderItemId, amount, bookId);
+			if(netAmount != null) {
+				orderItem.setNetAmount(netAmount);
 			}
-
-			// get the order item that was updated (to get access to the book ID)
-			String orderItemId = event.getParameterInfo().getQueryParameter(OrderItems.ID);
-			Result result = adminService.run(Select.from(OrderItems_.class).byId(orderItemId));
-			OrderItems itemToPatch = result.first(OrderItems.class).orElseThrow(notFound("OrderItem does not exist"));
-
-			// get the book for the book ID to calculate the net amount from the book price
-			result = adminService.run(Select.from(Books_.class).byId(itemToPatch.getBookId()));
-			Books book = result.first(Books.class).orElseThrow(notFound("Book does not exist"));
-
-			// update the net amount of the order item
-			BigDecimal updatedNetAmount = book.getPrice().multiply(BigDecimal.valueOf(amount));
-			orderItem.setNetAmount(updatedNetAmount);
 		});
 	}
 
+	@Before(event = DraftService.EVENT_DRAFT_CANCEL, entity = OrderItems_.CDS_NAME)
+	public void cancelOrderItems(DraftCancelEventContext event) {
+		String orderItemId = event.getParameterInfo().getQueryParameter(OrderItems.ID); // FIXME get from CQN
+		calculateNetAmountInDraft(orderItemId, 0, null);
+	}
+
+	private BigDecimal calculateNetAmountInDraft(String orderItemId, Integer newAmount, String newBookId) {
+		Integer amount = newAmount;
+		String bookId = newBookId;
+		if(amount == null && bookId == null) {
+			return null; // nothing changed
+		}
+
+		// get the order item that was updated (to get access to the book price, amount and order total)
+		Result result = adminService.run(Select.from(OrderItems_.class)
+				.columns(o -> o.amount(), o -> o.netAmount(),
+						o -> o.book().expand(b -> b.ID(), b -> b.price()),
+						o -> o.parent().expand(p -> p.ID(), p -> p.total()))
+				.where(o -> o.ID().eq(orderItemId).and(o.IsActiveEntity().eq(false))));
+		OrderItems itemToPatch = result.first(OrderItems.class).orElseThrow(notFound("OrderItem does not exist"));
+		BigDecimal bookPrice = null;
+
+		// fallback to existing values
+		if(amount == null) {
+			amount = itemToPatch.getAmount();
+		}
+
+		if(bookId == null) {
+			bookId = itemToPatch.getBook().getId();
+			bookPrice = itemToPatch.getBook().getPrice();
+		}
+
+		if(amount == null || bookId == null) {
+			return null; // not enough data available
+		}
+
+		// get the price of the updated book ID
+		if(bookPrice == null) {
+			result = adminService.run(Select.from(Books_.class).byId(bookId).columns(b -> b.price()));
+			Books book = result.first(Books.class).orElseThrow(notFound("Book does not exist"));
+			bookPrice = book.getPrice();
+		}
+
+		// update the net amount of the order item
+		BigDecimal updatedNetAmount = bookPrice.multiply(BigDecimal.valueOf(amount));
+
+		// update the order's total
+		BigDecimal previousNetAmount = defaultZero(itemToPatch.getNetAmount());
+		BigDecimal currentTotal = defaultZero(itemToPatch.getParent().getTotal());
+		BigDecimal newTotal = currentTotal.subtract(previousNetAmount).add(updatedNetAmount);
+		adminService.patchDraft(Update.entity(Orders_.class)
+				.where(o -> o.ID().eq(itemToPatch.getParent().getId()).and(o.IsActiveEntity().eq(false)))
+				.data(Orders.TOTAL, newTotal));
+
+		return updatedNetAmount;
+	}
 
 	private Supplier<ServiceException> notFound(String message) {
 		return () -> new ServiceException(ErrorStatuses.NOT_FOUND, message);
+	}
+
+	private BigDecimal defaultZero(BigDecimal decimal) {
+		return decimal == null ? BigDecimal.valueOf(0) : decimal;
 	}
 
 }
