8e8da3fe4d186000181c1f6c67d9e4e803315004
Christian Georgi <christian.georgi@sap.com>
2020-01-31 15:43:28 +0000

    Simplifation for OpenSAP
    - No draft
    - No auth
    - Only released version
    - package-lock

--- srv/admin-service.cds
+++ srv/admin-service.cds
@@ -1,12 +1,12 @@
 using { my.bookshop as my } from '../db/schema';
 
 @path:'admin'
-service AdminService @(requires:'admin') {
+service AdminService  {
   entity Books as projection on my.Books;
   entity Authors as projection on my.Authors;
   entity Orders as select from my.Orders;
 }
 
-// Enable Fiori Draft for Orders
-annotate AdminService.Orders with @odata.draft.enabled;
-annotate AdminService.Books with @Capabilities.Insertable: false;
+// // Enable Fiori Draft for Orders
+// annotate AdminService.Orders with @odata.draft.enabled;
+// annotate AdminService.Books with @Capabilities.Insertable: false;
--- srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
@@ -7,32 +7,24 @@ import java.math.BigDecimal;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.sap.cds.Result;
 import com.sap.cds.ql.Select;
 import com.sap.cds.ql.Update;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.cds.CdsService;
-import com.sap.cds.services.draft.DraftCancelEventContext;
-import com.sap.cds.services.draft.DraftPatchEventContext;
-import com.sap.cds.services.draft.DraftService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
-import com.sap.cds.services.messages.Messages;
+import com.sap.cds.services.persistence.PersistenceService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import cds.gen.adminservice.AdminService_;
 import cds.gen.adminservice.Books;
 import cds.gen.adminservice.Books_;
-import cds.gen.adminservice.OrderItems;
-import cds.gen.adminservice.OrderItems_;
 import cds.gen.adminservice.Orders;
-import cds.gen.adminservice.Orders_;
 import my.bookshop.MessageKeys;
 
 
@@ -45,13 +37,10 @@ import my.bookshop.MessageKeys;
 @ServiceName(AdminService_.CDS_NAME)
 public class AdminServiceHandler implements EventHandler {
 
-	@Resource(name = AdminService_.CDS_NAME)
-	private DraftService adminService;
-
 	@Autowired
-	private Messages messages;
+	PersistenceService db;
 
-	/**
+    /**
 	 * Finish an order
 	 * @param orders
 	 */
@@ -76,7 +65,7 @@ public class AdminServiceHandler implements EventHandler {
 				}
 
 				// check if enough books are available
-				Result result = adminService.run(Select.from(Books_.class).columns(b -> b.ID(), b -> b.stock(), b -> b.price()).byId(id));
+				Result result = db.run(Select.from(Books_.class).columns(b -> b.ID(), b -> b.stock(), b -> b.price()).byId(id));
 				Books book = result.first(Books.class).orElseThrow(notFound(MessageKeys.BOOK_MISSING));
 				if (book.getStock() < amount) {
 					throw new ServiceException(ErrorStatuses.BAD_REQUEST, MessageKeys.BOOK_REQUIRE_STOCK);
@@ -91,104 +80,13 @@ public class AdminServiceHandler implements EventHandler {
 				// update the book with the new stock
 				// FIXME this should only update the diff amount and handle book changes
 				book.setStock(book.getStock() - amount);
-				adminService.run(Update.entity(Books_.class).data(book));
+				db.run(Update.entity(Books_.class).data(book));
 			});
 		});
 	}
 
-	/**
-	 * Calculate the net amount and total preview updated while editing an order
-	 * @param context
-	 * @param orderItems
-	 */
-	@Before(event = DraftService.EVENT_DRAFT_PATCH)
-	public void patchOrderItems(DraftPatchEventContext context, Stream<OrderItems> orderItems) {
-		orderItems.forEach(orderItem -> {
-			// check if amount or book was updated
-			Integer amount = orderItem.getAmount();
-			String bookId = orderItem.getBookId();
-			String orderItemId = context.getParameterInfo().getQueryParameter(OrderItems.ID); // FIXME get from CQN
-			BigDecimal netAmount = calculateNetAmountInDraft(orderItemId, amount, bookId);
-			if(netAmount != null) {
-				orderItem.setNetAmount(netAmount);
-			}
-		});
-	}
-
-	/**
-	 * Calculate total preview when an order item is deleted
-	 * @param context
-	 */
-	@Before(event = DraftService.EVENT_DRAFT_CANCEL, entity = OrderItems_.CDS_NAME)
-	public void cancelOrderItems(DraftCancelEventContext context) {
-		String orderItemId = context.getParameterInfo().getQueryParameter(OrderItems.ID); // FIXME get from CQN
-		calculateNetAmountInDraft(orderItemId, 0, null);
-	}
-
-	private BigDecimal calculateNetAmountInDraft(String orderItemId, Integer newAmount, String newBookId) {
-		Integer amount = newAmount;
-		String bookId = newBookId;
-		if(amount == null && bookId == null) {
-			return null; // nothing changed
-		}
-
-		// get the order item that was updated (to get access to the book price, amount and order total)
-		Result result = adminService.run(Select.from(OrderItems_.class)
-				.columns(o -> o.amount(), o -> o.netAmount(),
-						o -> o.book().expand(b -> b.ID(), b -> b.price()),
-						o -> o.parent().expand(p -> p.ID(), p -> p.total()))
-				.where(o -> o.ID().eq(orderItemId).and(o.IsActiveEntity().eq(false))));
-		OrderItems itemToPatch = result.first(OrderItems.class).orElseThrow(notFound(MessageKeys.ORDERITEM_MISSING));
-		BigDecimal bookPrice = null;
-
-		// fallback to existing values
-		if(amount == null) {
-			amount = itemToPatch.getAmount();
-		}
-
-		if(bookId == null && itemToPatch.getBook() != null) {
-			bookId = itemToPatch.getBook().getId();
-			bookPrice = itemToPatch.getBook().getPrice();
-		}
-
-		if(amount == null || bookId == null) {
-			return null; // not enough data available
-		}
-
-		// only warn about invalid values in draft mode
-		if(amount <= 0) {
-			// additional messages with localized messages from property files
-			// these messages are transported in sap-messages and do not abort the request
-			messages.warn(MessageKeys.AMOUNT_REQUIRE_MINIMUM);
-		}
-
-		// get the price of the updated book ID
-		if(bookPrice == null) {
-			result = adminService.run(Select.from(Books_.class).byId(bookId).columns(b -> b.price()));
-			Books book = result.first(Books.class).orElseThrow(notFound(MessageKeys.BOOK_MISSING));
-			bookPrice = book.getPrice();
-		}
-
-		// update the net amount of the order item
-		BigDecimal updatedNetAmount = bookPrice.multiply(BigDecimal.valueOf(amount));
-
-		// update the order's total
-		BigDecimal previousNetAmount = defaultZero(itemToPatch.getNetAmount());
-		BigDecimal currentTotal = defaultZero(itemToPatch.getParent().getTotal());
-		BigDecimal newTotal = currentTotal.subtract(previousNetAmount).add(updatedNetAmount);
-		adminService.patchDraft(Update.entity(Orders_.class)
-				.where(o -> o.ID().eq(itemToPatch.getParent().getId()).and(o.IsActiveEntity().eq(false)))
-				.data(Orders.TOTAL, newTotal));
-
-		return updatedNetAmount;
-	}
-
 	private Supplier<ServiceException> notFound(String message) {
 		return () -> new ServiceException(ErrorStatuses.NOT_FOUND, message);
-	}
-
-	private BigDecimal defaultZero(BigDecimal decimal) {
-		return decimal == null ? BigDecimal.valueOf(0) : decimal;
 	}
 
 }
