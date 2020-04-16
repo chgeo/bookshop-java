4f8ed87683e2753c319641c5c9868bf655ccfb2c
Christian Georgi <christian.georgi@sap.com>
2020-04-16 18:37:52 +0200

    Fix tests

--- srv/src/test/java/my/bookshop/CatalogServiceTest.java
+++ srv/src/test/java/my/bookshop/CatalogServiceTest.java
@@ -6,9 +6,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,44 +14,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.sap.cds.ql.Insert;
-import com.sap.cds.services.persistence.PersistenceService;
-
-import cds.gen.my.bookshop.Books;
-import cds.gen.my.bookshop.Books_;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CatalogServiceTest {
 
-	@Autowired
-	PersistenceService db;
+	private static final String booksURI = "/api/browse/Books";
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Before
-	public void initializeBooks() {
-		Books book1 = Books.create();
-		book1.setTitle("Wuthering Heights");
-		book1.setStock(100);
-
-		Books book2 = Books.create();
-		book2.setTitle("Harry Potter and the Sorcerer's Stone");
-		book2.setStock(200);
-
-		db.run(Insert.into(Books_.class).entries(Arrays.asList(book1, book2)));
+	@Test
+	public void testDiscountApplied() throws Exception {
+		mockMvc.perform(get(booksURI + "?$filter=stock gt 200&top=1"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.value[0].title").value(containsString("11% discount")));
 	}
 
 	@Test
-	public void testDiscount() throws Exception {
-		mockMvc.perform(get("/api/browse/Books"))
+	public void testDiscountNotApplied() throws Exception {
+		mockMvc.perform(get(booksURI + "?$filter=stock lt 100&top=1"))
 		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.value[0].title").value(not(containsString("11% discount"))))
-		.andExpect(jsonPath("$.value[0].stock").value(100))
-		.andExpect(jsonPath("$.value[1].title").value(containsString("11% discount")))
-		.andExpect(jsonPath("$.value[1].stock").value(200));
+		.andExpect(jsonPath("$.value[0].title").value(not(containsString("11% discount"))));
 	}
 
 }
